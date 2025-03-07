b6539e259a8590b1bc0401d99dd160b9c7f146bb
LumpyAndLevi <gordontomas1@gmail.com>
2025-03-07 00:30:16 +0000

    what

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hi, what's up</p>>
+        <p>Hi, what's up</p>
       </header>
     </div>
   );
